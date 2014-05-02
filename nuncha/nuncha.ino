73ac9513d44a5a94b5040f7651ee43f49e413752
Marco Galardini <galactus8403@gmail.com>
2014-05-02 23:31:55 +0100

    Poperly combine the three synth together
    The synths are combined using a volume parameter, rhoughly equal to
    the roll/pitch.

--- nuncha/nuncha.ino
+++ nuncha/nuncha.ino
@@ -10,25 +10,27 @@
 #define UP 2
 
 int bumpy_input = 0;
+int constrain_input = 0;
 
 int rollpitch = 0;
 
 #include <Oscil.h> // oscillator template
 #include <tables/sin2048_int8.h> // sine table for oscillator
 #include <tables/triangle2048_int8.h> // triangle table for oscillator
-#include <tables/saw2048_int8.h> // saw table for oscillator
-#include <RollingAverage.h>
+#include <tables/triangle_analogue512_int8.h> // saw table for oscillator
 
 // use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
 Oscil <TRIANGLE2048_NUM_CELLS, AUDIO_RATE> aTri(TRIANGLE2048_DATA);
-Oscil <SAW2048_NUM_CELLS, AUDIO_RATE> aSaw(SAW2048_DATA);
+Oscil <TRIANGLE_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aSaw(TRIANGLE_ANALOGUE512_DATA);
 
+// Average the input signals
+#include <RollingAverage.h>
 // use: RollingAverage <number_type, how_many_to_average> myThing
 // how_many_to_average has to be power of 2
-RollingAverage <float, 8> rollLAverage;
-RollingAverage <float, 8> rollRAverage;
-RollingAverage <float, 8> pitchAverage;
+RollingAverage <float, 16> rollLAverage;
+RollingAverage <float, 16> rollRAverage;
+RollingAverage <float, 16> pitchAverage;
 RollingAverage <float, 4> xAverage;
 RollingAverage <float, 4> yAverage;
 int rollLeft;
@@ -37,12 +39,18 @@ int pitch;
 int xStick;
 int yStick;
 
+unsigned char volumeLeft;
+unsigned char volumeRight;
+unsigned char volumePitch;
+
 int play = 0;
 
 void setup(){
   uint8_t error;
   
   error = Nunchuck.begin ();
+  
+  //Serial.begin(19200);
   
   /*if ( error )
   {
@@ -54,9 +62,9 @@ void setup(){
   startMozzi(CONTROL_RATE); // set a control rate of 64 (powers of 2 please)
   
   // set the frequency
-  aSin.setFreq(440);
-  aTri.setFreq(440);
-  aSaw.setFreq(440);
+  aSin.setFreq(0);
+  aTri.setFreq(0);
+  aSaw.setFreq(0);
 }
 
 void updateControl(){
@@ -64,21 +72,35 @@ void updateControl(){
 
    Nunchuck.update(); 
    
-   if ( !Nunchuck.getZButton() )
+   //Serial.print(Nunchuck . getRaw()->yStick);
+   //Serial.print(' ');
+   //Serial.println((int)volume);
+   
+   if ( Nunchuck.getZButton() )
    { 
-     bumpy_input = constrain(Nunchuck . getRoll(), -180, -1);
-     bumpy_input = map(-bumpy_input, 0, 180, 0, 800);
+     constrain_input = constrain(Nunchuck . getRoll(), -180, -1);
+     bumpy_input = map(-constrain_input, 0, 180, 0, 800);
+     constrain_input = constrain(-constrain_input, 0, 100);
+     volumeLeft = map(constrain_input, 0, 100, 0, 255);
      rollLeft = rollLAverage.next(bumpy_input);
-     bumpy_input = constrain(Nunchuck . getRoll(), 0, 180);
-     bumpy_input = map(bumpy_input, 0, 180, 0, 800);
+     constrain_input = constrain(Nunchuck . getRoll(), 0, 180);
+     bumpy_input = map(constrain_input, 0, 180, 0, 800);
+     constrain_input = constrain(constrain_input, 0, 100);
+     volumeRight = map(constrain_input, 0, 100, 0, 255);
      rollRight = rollRAverage.next(bumpy_input);
-     bumpy_input = constrain(Nunchuck . getPitch(), 100, 174);
-     bumpy_input = map(bumpy_input, 100, 174, 0, 800);
+     constrain_input = constrain(Nunchuck . getPitch(), 100, 130);
+     bumpy_input = map(constrain_input, 100, 174, 0, 800);
+     constrain_input = constrain(constrain_input, 100, 144);
+     volumePitch = map(constrain_input, 100, 144, 0, 150);
      pitch = pitchAverage.next(bumpy_input);
+     
+     constrain_input = constrain(Nunchuck . getRaw()->yStick, 31, 129);
+     bumpy_input = map(-constrain_input, -129, -31, 200, 1000);
+     yStick = yAverage.next(bumpy_input);     
 
-     aSin.setFreq(rollLeft);
+     aSin.setFreq(pitch);
      aTri.setFreq(rollRight);
-     aSaw.setFreq(pitch);
+     aSaw.setFreq(rollLeft);
      
      play = 1;
    }
@@ -90,34 +112,8 @@ void updateControl(){
 int updateAudio(){
   if (play)
   {
-    if ( pitch >= rollLeft)
-    {
-      if ( pitch >= rollRight)
-      { return aSaw.next(); }
-      else
-      { return aTri.next(); }
-    }
-    else if ( pitch >= rollRight)
-    {
-      if ( pitch >= rollLeft)
-      { return aSaw.next(); }
-      else
-      { return aSin.next(); }
-    }
-    else if ( rollRight >= rollLeft)
-    {
-      if ( pitch >= rollRight)
-      { return aSaw.next(); }
-      else
-      { return aTri.next(); }
-    }
-    else if ( rollLeft >= rollRight)
-    {
-      if ( pitch >= rollLeft)
-      { return aSaw.next(); }
-      else
-      { return aSin.next(); }
-    }
+    int synth = (((int)aSaw.next() * volumeLeft) + ((int)aTri.next() * volumeRight) + ((int)aSin.next() * volumePitch))>>8;
+    return synth;
   }
   
   return 0;
