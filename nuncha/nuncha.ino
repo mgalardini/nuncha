0cfc5d7f8f03e433f79f7905fc7aee051163ee7d
Marco Galardini <galactus8403@gmail.com>
2014-05-26 20:30:44 +0100

    Working double envelope
    Main oscillator by pitch, octaves by rolling, filter by y stick
    Play note using the Z button
    
    Thanks to @brandoninvergo for the blending of different oscillators

--- nuncha/nuncha.ino
+++ nuncha/nuncha.ino
@@ -2,12 +2,10 @@
 #include "Nunchuck.h"
 #include <Wire.h> 
 #include <MozziGuts.h>
+#include <LowPassFilter.h>
+#include <ADSR.h>
 
-#define CONTROL_RATE 64 // powers of 2 please
-
-#define LEFT 0
-#define RIGHT 1
-#define UP 2
+#define CONTROL_RATE 128 // powers of 2 please
 
 int bumpy_input = 0;
 int constrain_input = 0;
@@ -16,13 +14,11 @@ int rollpitch = 0;
 
 #include <Oscil.h> // oscillator template
 #include <tables/sin2048_int8.h> // sine table for oscillator
-#include <tables/triangle2048_int8.h> // triangle table for oscillator
-#include <tables/triangle_analogue512_int8.h> // saw table for oscillator
 
 // use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
-Oscil <TRIANGLE2048_NUM_CELLS, AUDIO_RATE> aTri(TRIANGLE2048_DATA);
-Oscil <TRIANGLE_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aSaw(TRIANGLE_ANALOGUE512_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aRight(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aLeft(SIN2048_DATA);
 
 // Average the input signals
 #include <RollingAverage.h>
@@ -39,18 +35,30 @@ int pitch;
 int xStick;
 int yStick;
 
+LowPassFilter lpf;
+
+ADSR <CONTROL_RATE, CONTROL_RATE> envelope;
+ADSR <CONTROL_RATE, CONTROL_RATE> envelope2;
+
 unsigned char volumeLeft;
 unsigned char volumeRight;
 unsigned char volumePitch;
 
+int pressed = 0;
+int unpressed = 0;
 int play = 0;
+
+unsigned int duration, attack, decay, sustain, release_ms;
+
+byte gain = 0;
+byte gain2 = 0;
 
 void setup(){
   uint8_t error;
   
   error = Nunchuck.begin ();
   
-  //Serial.begin(19200);
+  //Serial.begin(9600);
   
   /*if ( error )
   {
@@ -63,60 +71,92 @@ void setup(){
   
   // set the frequency
   aSin.setFreq(0);
-  aTri.setFreq(0);
-  aSaw.setFreq(0);
+  aRight.setFreq(0);
+  aLeft.setFreq(0);
+  
+  attack = 50;
+  decay = 50;
+  sustain = 100;
+  release_ms = 500;
+  envelope.setADLevels(255, 100);
+  envelope.setTimes(attack,decay,sustain,release_ms);
+
+  envelope2.setADLevels(255, 100);
+  envelope2.setTimes(attack,decay,sustain,release_ms);
+
+  lpf.setResonance(1);
+  
+  lpf.setCutoffFreq(128);
 }
 
 void updateControl(){
-   // read the input device
-
    Nunchuck.update(); 
-   
-   //Serial.print(Nunchuck . getRaw()->yStick);
-   //Serial.print(' ');
-   //Serial.println((int)volume);
-   
-   if ( Nunchuck.getZButton() )
-   { 
+
+   if( Nunchuck.getZButton() )
+   {
+     pressed++;
+     if( pressed >= 1 )
+     {
+       envelope.noteOn();
+       envelope2.noteOn();
+       pressed = 0;
+     }
+   }
+
      constrain_input = constrain(Nunchuck . getRoll(), -180, -1);
-     bumpy_input = map(-constrain_input, 0, 180, 0, 800);
-     constrain_input = constrain(-constrain_input, 0, 100);
-     volumeLeft = map(constrain_input, 0, 100, 0, 255);
+     bumpy_input = map(-constrain_input, 0, 180, 11, 0);
      rollLeft = rollLAverage.next(bumpy_input);
+
      constrain_input = constrain(Nunchuck . getRoll(), 0, 180);
-     bumpy_input = map(constrain_input, 0, 180, 0, 800);
-     constrain_input = constrain(constrain_input, 0, 100);
-     volumeRight = map(constrain_input, 0, 100, 0, 255);
+     bumpy_input = map(constrain_input, 0, 180, 11, 0);
      rollRight = rollRAverage.next(bumpy_input);
-     constrain_input = constrain(Nunchuck . getPitch(), 100, 130);
-     bumpy_input = map(constrain_input, 100, 174, 0, 800);
-     constrain_input = constrain(constrain_input, 100, 144);
-     volumePitch = map(constrain_input, 100, 144, 0, 150);
+
+     constrain_input = constrain(Nunchuck . getPitch(), 18, 174);
+     bumpy_input = map(constrain_input, 100, 150, 440, 880);
      pitch = pitchAverage.next(bumpy_input);
      
      constrain_input = constrain(Nunchuck . getRaw()->yStick, 31, 129);
      bumpy_input = map(-constrain_input, -129, -31, 200, 1000);
      yStick = yAverage.next(bumpy_input);     
 
-     aSin.setFreq(pitch);
-     aTri.setFreq(rollRight);
-     aSaw.setFreq(rollLeft);
-     
-     play = 1;
-   }
-   
-   else{play = 0;}
+     aSin.setFreq((int)pitch);
+     if (rollRight == 0 )
+     {
+       aRight.setFreq(0);
+     }
+     else
+     {
+       aRight.setFreq((int)((2*pitch) + rollRight*73));
+       //aRight.setFreq((int)((2*pitch) - rollRight*73));
+     }
+     if (rollLeft == 0 )
+     {
+       aLeft.setFreq(0);
+     }
+     else{
+       aLeft.setFreq( (int)((pitch/2)+ 18*rollLeft));
+       //aLeft.setFreq( (int)((pitch/2)- 18*rollLeft));
+     }
+   envelope.update();
+   envelope2.update();  
+ 
+   gain = envelope.next();
+   gain2 = envelope2.next();
 }
 
 
 int updateAudio(){
-  if (play)
-  {
-    int synth = (((int)aSaw.next() * volumeLeft) + ((int)aTri.next() * volumeRight) + ((int)aSin.next() * volumePitch))>>8;
+    int synth;
+    
+    if( rollRight == 0 )
+    {
+      synth = lpf.next(((long)gain * aSin.next() + (int)gain2 * aLeft.next()) >> 9 );
+    }
+    else
+    {
+      synth = lpf.next(((long)gain * aSin.next() + (int)gain2 * aRight.next()) >> 9);
+    }
     return synth;
-  }
-  
-  return 0;
 }
 
 void loop(){
