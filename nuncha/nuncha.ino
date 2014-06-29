4e09e3ba8a6b08b1b5cb635ee870832b96b04be6
Marco Galardini <galactus8403@gmail.com>
2014-06-29 20:02:37 +0100

    Better sound!
    * Z-button uses a triangle oscillator
    * Same envelope for both buttons
    * Left/Right control a vibrato effect

--- nuncha/nuncha.ino
+++ nuncha/nuncha.ino
@@ -7,11 +7,16 @@
 #define CONTROL_RATE 128 // powers of 2 please
 #include <Oscil.h> // oscillator template
 #include <tables/sin2048_int8.h> // sine table for oscillator
+#include <tables/triangle2048_int8.h> // saw table for oscillator
 
 // use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aRight(SIN2048_DATA);
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aLeft(SIN2048_DATA);
+
+Oscil <TRIANGLE2048_NUM_CELLS, AUDIO_RATE> aSin1(TRIANGLE2048_DATA);
+Oscil <TRIANGLE2048_NUM_CELLS, AUDIO_RATE> aRight1(TRIANGLE2048_DATA);
+Oscil <TRIANGLE2048_NUM_CELLS, AUDIO_RATE> aLeft1(TRIANGLE2048_DATA);
 
 // Average the input signals
 #include <RollingAverage.h>
@@ -27,9 +32,6 @@ int pitch;
 
 ADSR <CONTROL_RATE, CONTROL_RATE> envelope;
 ADSR <CONTROL_RATE, CONTROL_RATE> envelope2;
-
-ADSR <CONTROL_RATE, CONTROL_RATE> envelope3;
-ADSR <CONTROL_RATE, CONTROL_RATE> envelope4;
 
 int zpressed = 0;
 int cpressed = 0;
@@ -61,26 +63,19 @@ void setup(){
   aSin.setFreq(0);
   aRight.setFreq(0);
   aLeft.setFreq(0);
+  aSin1.setFreq(0);
+  aRight1.setFreq(0);
+  aLeft1.setFreq(0);
   
   attack = 50;
   decay = 50;
-  sustain = 100;
-  release_ms = 500;
+  sustain = 200;
+  release_ms = 400;
   envelope.setADLevels(255, 100);
   envelope.setTimes(attack,decay,sustain,release_ms);
 
   envelope2.setADLevels(255, 100);
   envelope2.setTimes(attack,decay,sustain,release_ms);
-
-  attack = 100;
-  decay = 200;
-  sustain = 500;
-  release_ms = 1000;
-  envelope3.setADLevels(255, 100);
-  envelope3.setTimes(attack,decay,sustain,release_ms);
-
-  envelope4.setADLevels(255, 100);
-  envelope4.setTimes(attack,decay,sustain,release_ms);
 }
 
 void updateControl(){
@@ -104,8 +99,8 @@ void updateControl(){
      cpressed++;
      if( cpressed >= 1 )
      {
-       envelope3.noteOn();
-       envelope4.noteOn();
+       envelope.noteOn();
+       envelope2.noteOn();
        cpressed = 0;
        cnote = 1;
        znote = 0;
@@ -122,11 +117,11 @@ void updateControl(){
    bool stickRt = ( xStick >   StickThresh ? true : false );
    
      int Linput = constrain(Nunchuck . getRoll(), -180, -1);
-     int BLinput = map(-Linput, 1, 180, 1, 33);
+     int BLinput = map(-Linput, 1, 180, 0, 400);
      rollLeft = rollLAverage.next(BLinput);
 
      int Rinput = constrain(Nunchuck . getRoll(), 0, 180);
-     int BRinput = map(Rinput, 0, 180, 1, 33);
+     int BRinput = map(Rinput, 0, 180, 0, 400);
      rollRight = rollRAverage.next(BRinput);
 
      int constrain_input = constrain(Nunchuck . getPitch(), 18, 174);
@@ -155,42 +150,58 @@ void updateControl(){
      if (rollLeft > rollRight )
      {
        aRight.setFreq(0);
-        aLeft.setFreq( (int)((pitch/2) + rollLeft));
+       aLeft.setFreq( rollLeft );
      }
      else
      {
        aLeft.setFreq(0);
-       aRight.setFreq((int)((pitch/2) + rollRight));
+       aRight.setFreq( rollRight );
      }
+     
+     aSin1.setFreq((int)pitch);   
+     if (rollLeft > rollRight )
+     {
+       aRight1.setFreq(0);
+       aLeft1.setFreq( rollLeft );
+     }
+     else
+     {
+       aLeft1.setFreq(0);
+       aRight1.setFreq( rollRight );
+     }
+
    envelope.update();
    envelope2.update();  
-   envelope3.update();
-   envelope4.update();  
    
-   if( znote )
-   {
-     gain = envelope.next();
-     gain2 = envelope2.next();
-   }
-   else
-   {
-     gain = envelope3.next();
-     gain2 = envelope4.next();
-   }
-
+   gain = envelope.next();
+   gain2 = envelope2.next();
 }
 
 
 int updateAudio(){
     int synth;
-    
+
     if( rollLeft > rollRight )
     {
-      synth = ((long)gain * aSin.next() + (int)gain2 * aLeft.next()) >> 9 ;
+      if( cnote ){
+        Q15n16 vibrato = (Q15n16) 200 * aLeft.next();
+        synth = (long)gain * aSin.phMod(vibrato) >> 8;
+      }
+      else{
+        Q15n16 vibrato = (Q15n16) 200 * aLeft1.next();
+        synth = (long)gain * aSin1.phMod(vibrato) >> 8;
+      }
     }
     else
     {
-      synth = ((long)gain * aSin.next() + (int)gain2 * aRight.next()) >> 9;
+      if( cnote ){
+        Q15n16 vibrato = (Q15n16) 200 * aRight.next();
+        synth = (long)gain * aSin.phMod(vibrato) >> 8;
+      }
+      else{
+        Q15n16 vibrato = (Q15n16) 200 * aRight1.next();
+        synth = (long)gain * aSin1.phMod(vibrato) >> 8;
+      }
     }
     return synth;
 }
